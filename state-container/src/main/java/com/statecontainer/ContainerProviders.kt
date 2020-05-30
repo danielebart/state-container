dc8560cdfaba849288aa0d21e6b0bd050d60dbd0
Daniele Bartorilla <danielebartorilla@gmail.com>
2020-05-30 17:51:18 +0200

    add no-lazy container provider

--- state-container/src/main/java/com/statecontainer/ContainerProviders.kt
+++ state-container/src/main/java/com/statecontainer/ContainerProviders.kt
@@ -12,25 +12,35 @@ import androidx.savedstate.SavedStateRegistry
 inline fun <P : StateContainer, reified S> provideContainerLazily(
     savedStateRegistry: SavedStateRegistry,
     noinline containerFactory: (stateSaver: StateSaver<S>) -> P
-): Lazy<P> = provideContainerLazily(S::class.java, savedStateRegistry, containerFactory)
+): Lazy<P> = lazy { provideContainer(S::class.java, savedStateRegistry, containerFactory) }
 
 /**
- * Provides in a lazy-way a [StateContainer] from a factory lambda and a [SavedStateRegistry].
+ * Provides a [StateContainer] from a factory lambda and a [SavedStateRegistry].
+ *
+ * @param savedStateRegistry the [SavedStateRegistry] used to save the state. This object can be
+ * easily obtained from any SavedStateRegistryOwner (Fragments, ComponentActivity, ...).
+ * @param containerFactory a [StateContainer] should be provided through this lambda.
+ */
+inline fun <P : StateContainer, reified S> provideContainer(
+    savedStateRegistry: SavedStateRegistry,
+    noinline containerFactory: (stateSaver: StateSaver<S>) -> P
+): P = provideContainer(S::class.java, savedStateRegistry, containerFactory)
+
+/**
+ * Provides a [StateContainer] from a factory lambda and a [SavedStateRegistry].
  *
  * @param stateClass the state class.
  * @param savedStateRegistry the [SavedStateRegistry] used to save the state. This object can be
  * easily obtained from any SavedStateRegistryOwner (Fragments, ComponentActivity, ...).
  * @param containerFactory a [StateContainer] should be provided through this lambda.
  */
-fun <P : StateContainer, S> provideContainerLazily(
+fun <P : StateContainer, S> provideContainer(
     stateClass: Class<S>,
     savedStateRegistry: SavedStateRegistry,
     containerFactory: (stateSaver: StateSaver<S>) -> P
-): Lazy<P> = lazy {
-    containerFactory(
-        AndroidLifecycleStateSaver(
-            containerKey = stateClass.name,
-            savedStateRegistry = savedStateRegistry
-        )
+): P = containerFactory(
+    AndroidLifecycleStateSaver(
+        containerKey = stateClass.name,
+        savedStateRegistry = savedStateRegistry
     )
-}
+)
